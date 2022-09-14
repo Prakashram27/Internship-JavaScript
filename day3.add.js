7ec0edd468f7a72599951e759dbd6e1b33940207
Prakash Ramu <108420815+Prakashram27@users.noreply.github.com>
2022-09-14 10:47:17 +0530

    Add files via upload

---
+++ day3.add.js
@@ -0,0 +1,14 @@
+export function double(a,b){
+    return a+b
+}
+
+
+export var name = "Prakash Ramu"
+
+export var person =  {
+    firstName : "Prakash",
+    lastName : "Ramu"
+}
+
+export var Fruitlist = ["apple","orange","banana", "strawberry"]
+
---
+++ day3main.js
@@ -0,0 +1,8 @@
+
+import {person,name,Fruitlist} from "./day3.add.js"
+        console.log(name)
+        console.log(person)
+        console.log(Fruitlist)
+
+
+
